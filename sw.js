a3da8907585ae2f04e6039d619230b4715496d1c
Jesse Do <previousdolphin@users.noreply.github.com>
2026-04-17 01:40:58 -0400

    Create sw.js
    sw for dietcoke PWA

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'dietcoke-oracle-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo192.PNG',
+  './logo512.PNG'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      // Intentionally using catch to prevent the entire caching process
+      // from failing if a logo isn't present right away.
+      return cache.addAll(ASSETS).catch((err) => console.log('Partial cache setup:', err));
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
